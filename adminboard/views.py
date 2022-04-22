fdcd965bdb1a217ee43c8179f1c24f94f7834343
nsr-g-japan <n.basha@g-japan.com>
2022-04-22 14:26:47 +0530

    sampledemo

--- adminboard/views.py
+++ adminboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def homepage(request):
-     return render(request, 'homepage.html',{})
+     return HttpResponse("<h1> Welcome to Global Japan Network</>")
